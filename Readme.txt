6acc7da703a5e3832855dbc73395476941b0c68e
TinaS2024 <Tina.Schmidtbauer@bolasys.de>
2025-04-23 10:50:55 +0200

    Datenbank erstellt und verbunden

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,3 @@
-Tutorial https://www.youtube.com/watch?v=XjMWSwoAOQc&t=109s
+Tutorial 7:22: https://www.youtube.com/watch?v=XjMWSwoAOQc&t=109s
+
+https://docs.medusajs.com/learn/installation
--- medusa-tutorial/medusa-config.ts
+++ medusa-tutorial/medusa-config.ts
@@ -1,16 +1,28 @@
 import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { resolve } from 'path'
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    http:{ 
+    storeCors: process.env.STORE_CORS!,
+    adminCors: process.env.ADMIN_CORS!,
+    authCors: process.env.AUTH_CORS!,
+    jwtSecret: process.env.JWT_SECRET || "supersecret",
+    cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
-  }
+  },
+  plugins: [
+  {
+    resolve: "medusa-storage-supabase",
+    options: {
+      referenceID: process.env.STORAGE_BUCKET_REF,
+      serviceKey: process.env.STORAGE_SERVICE_KEY,
+      bucketName: process.env.STORAGE_BUCKET_NAME,
+    },
+  },
+]
 })
+
