bc88739a89cedfb970b823f1f5fe0825250db095
TinaS2024 <Tina.Schmidtbauer@bolasys.de>
2026-04-13 10:15:37 +0200

    Vorbereitung Übersetzungs-Modul

--- medusa-config.ts
+++ medusa-config.ts
@@ -19,6 +19,12 @@ module.exports = defineConfig({
     {
       resolve: "./src/modules/bundled-product",
     },
+    {
+      resolve: "@medusajs/translation",
+    }
   ],
+  featureFlags: {
+    translation: true,
+  },
 })
 
